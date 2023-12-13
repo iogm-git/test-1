4afbefb9c0b5b0f222dde32652191fc62ff6432d
ilhamrhmtkbr@gmail.com <ilhamrhmtkbr@gmail.com>
2023-12-13 12:03:21 +0700

    edit layout

--- resources/views/main-layout.blade.php
+++ resources/views/main-layout.blade.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $page }} - {{ $style }}</title>
     <link rel="stylesheet" href="{{ asset('assets/css') }}/root.css">
     @if (request()->is('/') or request()->is('login'))
         <link rel="stylesheet" href="{{ asset('assets/css') }}/{{ $page }}.css">
+        <title>{{ $page }}</title>
     @else
+        <title>{{ $page }} - {{ $style }}</title>
         <link rel="stylesheet" href="{{ asset('assets/css/management') }}/{{ $style }}.css">
     @endif
 </head>
